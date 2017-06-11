fc6c315b0f38acb258b9c426994bbc36ec1d471f
I-NOZex <simplesmente_moi@hotmail.com>
2017-06-11 22:47:45 +0100

    Error: Task heroku: can't support dependencies that is not an array of strings

--- gulpfile.js
+++ gulpfile.js
@@ -133,7 +133,7 @@ gulp.task('release', ['clean', 'copy-images', 'copy-vendors', 'html-minify', 'sc
 //development task
 gulp.task('start', ['release', 'watch']);
 
-gulp.task('heroku:','release', function() {
+gulp.task('heroku:',['release'], function() {
   connect.server({
     root: prodPaths.root,
     port: process.env.PORT || 5000, // localhost:5000
